d745df03018be432e5ff1ae1d2e52dfd8b7312c4
glegshot <sreekumar.y2k@gmail.com>
2021-01-11 22:46:22 +0530

    added dependencies and handler

---
+++ src/main/java/org/example/Handler.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import com.amazonaws.services.lambda.runtime.Context;
+
+public class Handler{
+
+    public String handleRequest(String input, Context context){
+
+        context.getLogger().log("Input: " + input);
+        return "Hello World - " + input;
+    }
+
+
+}
